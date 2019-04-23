1c8c5f3a798b2f9ea3d8be6d42d17931f51463a8
Varonss <martin.varona.84@gmail.com>
2019-04-23 18:20:23 -0500

    Adding client's dist and src files

---
+++ client/dist/bundle.js
@@ -0,0 +1,111 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./client/src/Index.js":
+/*!*****************************!*\
+  !*** ./client/src/Index.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+    eval("throw new Error(\"Module build failed (from ./node_modules/eslint-loader/index.js):\\nError: No ESLint configuration found.\\n    at Config.getLocalConfigHierarchy (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint/lib/config.js:255:39)\\n    at Config.getConfigHierarchy (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint/lib/config.js:179:43)\\n    at Config.getConfigVector (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint/lib/config.js:286:21)\\n    at Config.getConfig (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint/lib/config.js:329:29)\\n    at processText (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint/lib/cli-engine.js:161:33)\\n    at CLIEngine.executeOnText (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint/lib/cli-engine.js:595:17)\\n    at lint (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint-loader/index.js:253:17)\\n    at Object.module.exports (/home/richie/Documents/Challenges/latest_boilerplate/node_modules/eslint-loader/index.js:248:21)\");\n\n//# sourceURL=webpack:///./client/src/Index.js?");
+
+    /***/ }),
+    
+    /***/ 0:
+    /*!***********************************!*\
+      !*** multi ./client/src/Index.js ***!
+      \***********************************/
+    /*! no static exports found */
+    /***/ (function(module, exports, __webpack_require__) {
+    
+    eval("module.exports = __webpack_require__(/*! ./client/src/Index.js */\"./client/src/Index.js\");\n\n\n//# sourceURL=webpack:///multi_./client/src/Index.js?");
+    
+    /***/ })
+    
+    /******/ });
